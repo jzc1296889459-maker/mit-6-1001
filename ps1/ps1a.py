31147dae68b60f6815a3f928eef52deea62a10f4
James Tseung <JZC1296889459@gmail.com>
2026-02-22 11:11:12 +1100

    PS1a: implement months calculation

--- ps1/ps1a.py
+++ ps1/ps1a.py
@@ -6,19 +6,27 @@
 ##################################################################################
 ## Get user input for yearly_salary, portion_saved and cost_of_dream_home below ##
 ##################################################################################
-while True:
-    yearly_salary = input("Enter your yearly salary: ")
-    try:
-        x = float(yearly_salary)
-        break
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
+yearly_salary = float(input("Enter your yearly salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+cost_of_dream_home = float(input("Enter the cost of your dream home: "))
+   
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
-
-
+portion_down_payment = 0.25
+r = 0.05
+amount_saved = 0
+down_payment = cost_of_dream_home*portion_down_payment
+months = 0
+monthly_salary = yearly_salary/12
+saving_of_month = monthly_salary * portion_saved
 ###############################################################################################
 ## Determine how many months it would take to get the down payment for your dream home below ## 
 ###############################################################################################
+
+while amount_saved < down_payment:
+    months = months + 1
+    amount_saved = amount_saved + saving_of_month + amount_saved *(r/12)
+
+print("Number of months: ", months)
